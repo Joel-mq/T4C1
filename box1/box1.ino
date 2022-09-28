44c91f7317530d49436d3df19e8a8b6d47fa3cc1
Joel-mq <joel.woodhouse@students.mq.edu.au>
2022-09-28 15:03:04 +1000

    added basic servo compatability + rewording of some variables/comments, photoresistors confirmed working

--- box1/box1.ino
+++ box1/box1.ino
@@ -1,8 +1,25 @@
+#include <Servo.h>
 #include <stdio.h>
+
+Servo myservo;
+
 
 // pr : photoresistor
 // vb : vibration sensor
 // bp : bumper
+
+// PINS
+
+/* SERVO PINS *//*
+  8: Servo1
+  9: Servo2
+
+/* ANALOG PINS *//*
+  A1: button1 (photoresistor)
+  A2: button2 (photoresistor)
+    A3: sensor (photoresistor)
+*/
+
 
 // all time units in ms
 unsigned long time = 0;
@@ -22,17 +39,23 @@ unsigned long timeSinceLastButtonInput = 0;
 
 // buttons (which are actually photoresistors but for 
 //          the sake of readability they are buttons)
-int pr1Output = 0;
-int pr2Output = 0;
-const int pr1 = A0;
-const int pr2 = A1;
+int button1Output = 0;
+int button2Output = 0;
+const int bt1 = A0;
+const int bt2 = A1;
 
 bool button1IsPressed = false;
 bool button2IsPressed = false;
 
 // not const as we can manually adjust for light levels later
 // example, very dark room
-int paddleLightSensitivity = 300;
+
+// TODO (?): lightMAX can be changed to account for maximum value
+// reached by the analog pin, which can be used to activate the paddle
+// after reaching (example) 1/2 of lightMAX, for now I am just using lightMIN
+// in the code
+int lightMAX = 300;
+int lightMIN = 100;
 
 
 // photoresistor
@@ -49,7 +72,11 @@ const int vb3 = 13;
 const int bp1LEDs = 10;
 
 
-// motors
+// Servos
+
+
+
+int openAngle;
 
 
 // Timer Event function
@@ -59,7 +86,7 @@ void timerEvent(void (*function)(bool), unsigned long *clock,
   
   if (start <= *clock && *clock <= stop) {
     function(condition);
-    Serial.println(*clock);
+    //Serial.println(*clock);
   }
   if (*clock > reset) {
     *clock = 0;
@@ -72,6 +99,8 @@ void setup()
   pinMode(vb1, INPUT);
   pinMode(vb2, INPUT);
   pinMode(vb3, INPUT);
+  myservo.attach(8);
+  //servo2.attach(9);
 }
 
 
@@ -83,13 +112,14 @@ void loop()
   // other timer stuff
   addTime(&bp1Timer);
   
-  pr1Output = analogRead(A0);
-  pr2Output = analogRead(A1);
-  movePaddle(pr1Output < paddleLightSensitivity, 8, &button1IsPressed);
-  movePaddle(pr2Output < paddleLightSensitivity, 9, &button2IsPressed);
+  button1Output = analogRead(A1);
+  //button2Output = analogRead(A2);
+  //Serial.println(analogRead(A1));
+  movePaddle(button1Output < lightMIN, 1, &button1IsPressed);
+  //movePaddle(button2Output < lightMIN, &servo2, &button2IsPressed);
   
   
-  timerEvent(&test, &bp1Timer, (digitalRead(vb1) == HIGH), 300, 800, 5000);
+  //timerEvent(&test, &bp1Timer, (digitalRead(vb1) == HIGH), 300, 800, 5000);
   
   
   
@@ -103,9 +133,9 @@ void loop()
 
 void test(bool condition) {
   if (condition) {
-    Serial.println("test success - true"); 
+    Serial.println("test success"); 
   } else {
-    Serial.println("test success - false"); 
+    Serial.println("test failure"); 
   }
 }
 
@@ -113,22 +143,18 @@ void test(bool condition) {
 // movePaddle
 // *buttonState is required make sure the code runs once, meaning a paddle
 // can move to it's target location without spamming code
-void movePaddle(bool input, int output, bool *buttonState) {
+void movePaddle(bool input, int temp, bool *buttonState) {
   if (input && !*buttonState) {
-    Serial.println("Input recieved, set pin: " + (String)output + " to HIGH"); 
+    Serial.println("Input recieved, set pin: to HIGH"); 
     *buttonState = true;
     
-    /*
-      ADD MOTOR STUFF HERE
-    */
+    myservo.write(180);
   }
   if (!input && *buttonState) {
-    Serial.println("No input recieved, set pin: " + (String)output + " to LOW"); 
+    Serial.println("No input recieved, set pin: to LOW"); 
     *buttonState = false;
     
-    /*
-      ADD MORE MOTOR STUFF HERE
-    */
+    myservo.write(0);
   }
 }
 
@@ -136,8 +162,6 @@ void movePaddle(bool input, int output, bool *buttonState) {
 void bumper(int input, int output, int timeActive) {
   
 }
-
-//
 
 
 
@@ -155,6 +179,7 @@ void blinkPin(int start, int stop, int mod, int PIN) {
     digitalWrite(PIN, HIGH);
   }
 }
+
 
 // time stuff
 void updateTime() {
